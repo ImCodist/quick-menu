411bb0412137be19c211e03f8f748348715ea9c6
ImCodist <50346006+ImCodist@users.noreply.github.com>
2023-06-29 11:43:39 -0400

    Organized the config menu a little.

--- src/main/java/xyz/imcodist/other/ModConfigModel.java
+++ src/main/java/xyz/imcodist/other/ModConfigModel.java
@@ -3,6 +3,7 @@ package xyz.imcodist.other;
 import io.wispforest.owo.config.annotation.Config;
 import io.wispforest.owo.config.annotation.Modmenu;
 import io.wispforest.owo.config.annotation.RangeConstraint;
+import io.wispforest.owo.config.annotation.SectionHeader;
 
 @Modmenu(modId = "quickmenu")
 @Config(name = "quickmenu", wrapperName = "ModConfig")
@@ -11,14 +12,16 @@ public class ModConfigModel {
         ALWAYS, KEYBIND_ONLY, NEVER
     }
 
+    @SectionHeader("menu")
     @RangeConstraint(min = 60, max = 150*3)
     public int menuWidth = 180;
     @RangeConstraint(min = 60, max = 150*3)
     public int menuHeight = 114;
 
-    public DisplayRunText displayRunText = DisplayRunText.KEYBIND_ONLY;
-
-    public boolean closeOnAction = true;
     public boolean closeOnKeyReleased = false;
+
+    @SectionHeader("action_buttons")
+    public boolean closeOnAction = true;
     public boolean showActionsInTooltip = true;
+    public DisplayRunText displayRunText = DisplayRunText.KEYBIND_ONLY;
 }
